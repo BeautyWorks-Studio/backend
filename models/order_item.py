fea2b9760e018d6ba7b3bbee1f0cc8a5a44908d5
Samuel <samuel.mbogo@student.moringaschool.com>
2025-07-23 22:34:51 +0300

    added models

---
+++ models/order_item.py
@@ -0,0 +1,6 @@
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey("order.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey("product.id"))
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
