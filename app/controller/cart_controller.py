aafde909274633f20530eae28f69f2c861b1ac57
Jesse <jesse.yegon@student.moringaschool.com>
2025-07-30 22:20:49 +0300

    addition of controllers

---
+++ app/controller/cart_controller.py
@@ -0,0 +1,43 @@
+from flask import request, jsonify
+from app.models.user import User
+from app.middleware.auth import token_required
+
+@token_required
+def get_cart():
+    user = User.objects(id=request.user_id).first()
+    if not user:
+        return jsonify({"success": False, "message": "User not found"}), 404
+    return jsonify({"success": True, "cartData": user.cart_data})
+
+@token_required
+def add_to_cart():
+    data = request.get_json()
+    item_id = data.get("itemId")
+    size = data.get("size")
+
+    user = User.objects(id=request.user_id).first()
+    cart = user.cart_data
+
+    if item_id not in cart:
+        cart[item_id] = {}
+    cart[item_id][size] = cart[item_id].get(size, 0) + 1
+
+    user.cart_data = cart
+    user.save()
+    return jsonify({"success": True, "message": "Added to cart"})
+
+@token_required
+def update_cart():
+    data = request.get_json()
+    item_id = data.get("itemId")
+    size = data.get("size")
+    quantity = data.get("quantity")
+
+    user = User.objects(id=request.user_id).first()
+    cart = user.cart_data
+    if item_id in cart and size in cart[item_id]:
+        cart[item_id][size] = quantity
+        user.cart_data = cart
+        user.save()
+        return jsonify({"success": True, "message": "Cart updated"})
+    return jsonify({"success": False, "message": "Item not found"}), 404
