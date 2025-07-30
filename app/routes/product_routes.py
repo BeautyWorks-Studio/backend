3346cbde597a54687f094e154296ae8475c3d4c6
Jesse <jesse.yegon@student.moringaschool.com>
2025-07-30 22:52:29 +0300

    addition of files

--- app/routes/product_routes.py
+++ app/routes/product_routes.py
@@ -1,55 +1,27 @@
 from flask import Blueprint, request, jsonify
-from models import Product
-from extensions import db
-from flask_jwt_extended import jwt_required
-from app.utils.decorators import role_required
+from app.middleware.admin_auth import admin_auth
+from app.middleware.upload_middleware import handle_uploads
+from app.controllers.product_controller import (
+    list_products, add_product, remove_product, single_product
+)
 
-product_bp = Blueprint('product_bp', __name__)
+product_bp = Blueprint('product', __name__, url_prefix='/api/products')
 
-@product_bp.route('/', methods=['GET'])
-def get_products():
-    products = Product.query.all()
-    return jsonify([{
-        "id": p.id,
-        "name": p.name,
-        "price": p.price,
-        "image": p.image,
-        "stock": p.stock
-    } for p in products])
+@product_bp.route('/add', methods=['POST'])
+@admin_auth
+@handle_uploads(['image1', 'image2', 'image3', 'image4'])
+def add_product_route():
+    return add_product(request)
 
-@product_bp.route('/', methods=['POST'])
-@jwt_required()
-@role_required(['admin', 'staff'])
-def create_product():
-    data = request.get_json()
-    new_product = Product(
-        name=data['name'],
-        price=data['price'],
-        image=data.get('image', ''),
-        stock=data.get('stock', 0)
-    )
-    db.session.add(new_product)
-    db.session.commit()
-    return jsonify({"msg": "Product added"}), 201
+@product_bp.route('/remove', methods=['POST'])
+@admin_auth
+def remove_product_route():
+    return remove_product(request)
 
-@product_bp.route('/<int:id>', methods=['PUT'])
-@jwt_required()
-@role_required(['admin', 'staff'])
-def update_product(id):
-    product = Product.query.get_or_404(id)
-    data = request.get_json()
-    product.name = data['name']
-    product.price = data['price']
-    product.image = data.get('image', product.image)
-    product.stock = data.get('stock', product.stock)
-    db.session.commit()
-    return jsonify({"msg": "Product updated"})
+@product_bp.route('/single', methods=['POST'])
+def single_product_route():
+    return single_product(request)
 
-@product_bp.route('/<int:id>', methods=['DELETE'])
-@jwt_required()
-@role_required(['admin', 'staff'])
-def delete_product(id):
-    product = Product.query.get_or_404(id)
-    db.session.delete(product)
-    db.session.commit()
-    return jsonify({"msg": "Product deleted"})
+@product_bp.route('/list', methods=['GET'])
+def list_products_route():
+    return list_products()
